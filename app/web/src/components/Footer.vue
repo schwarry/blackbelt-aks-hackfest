54d2ce5c1e428a67656f6c8175b83725abf6dbbf
schwarry <33959512+schwarry@users.noreply.github.com>
2019-02-10 18:14:22 -0800

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Kubernetes BJJ Black Belt changed
+        Don't give out your credit card to anyone!!!
       </div>
       <div class="col-lg-6">
       </div>
